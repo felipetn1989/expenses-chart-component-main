490013e0b13da8aa69a3e8fdb5f49017aacec2d7
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-03-09 10:11:41 -0300

    finished mobile version

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,22 +1,28 @@
-/* ../data.json */
-
 async function insertChart() {
   const response = await fetch("../data.json");
   const text = await response.text();
   const data = JSON.parse(text);
 
+  let maxExpense = data.reduce((max, obj) => Math.max(max, obj.amount), 0);
+
   for (let i = 0; i < data.length; i++) {
     chart.innerHTML += `
-        <div class="flex flex-col items-center">
-          <div class="w-7 h-10 bg-[#ec775f] rounded-sm">
-  
-          </div>
-          <span class="text-[0.625rem] text-[#382314]">
-            mon
-          </span>
-        </div>
-      `;
+    <div class="flex flex-col gap-3.5 items-center">
+      <div class="chart_bar w-[2.0625rem] bg-[#ec775f] rounded-sm">
+
+      </div>
+      <span class="text-[0.625rem] text-[#382314]">
+        ${data[i].day}
+      </span>
+    </div>
+  `;
   }
+
+  const chartBars = document.querySelectorAll(".chart_bar")
+
+  chartBars.forEach((bar,index) => {
+    bar.style.height = `${9.1875 * (data[index].amount/maxExpense)}rem`
+  })
 }
 
 insertChart();
